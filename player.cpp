f50217ffdaa46cb10199e9834c23d053af7104b3
finbel <oregan@kth.se>
2017-10-11 13:51:08 +0200

    beatifying

--- player.cpp
+++ player.cpp
@@ -34,11 +34,11 @@ GameState Player::play(const GameState &pState, const Deadline &pDue)
      * next state. This skeleton returns a random move instead.
      */
 
-	int bestValue = numeric_limits<int>::min();
+    int bestValue = numeric_limits<int>::min();
     int stateValue;
     int bestStateIdx = -1;
-	int alpha = numeric_limits<int>::min();
-	int beta = numeric_limits<int>::max();
+    int alpha = numeric_limits<int>::min();
+    int beta = numeric_limits<int>::max();
 
     int searchDepth = 5;
 
@@ -125,27 +125,12 @@ int Player::utility(const GameState &state)
 {
     int player = CELL_X;
     int notplayer = CELL_O;
-    //sum of the number of marks in each row, column, diagonals that do not
-    //contain any of the opponents marks.
-    // if (state.getNextPlayer() == CELL_O)
-    // {
-    //     //current player is X
-    //     player = CELL_X;
-    //     notplayer = CELL_O;
-    // }
-    // else
-    // {
-    //     // current player is O
-    //     player = CELL_O;
-    //     notplayer = CELL_X;
-    // }
-    //Lists to keep score for X
-    vector<int> differentSumsX(10, 0);
-    vector<bool> nonNulledLineX(10, false);
+    vector<int> occupiedX(10, 0);
+    vector<bool> blockedX(10, false);
 
     //Lists to keep score for O
-    vector<int> differentSumsO(10, 0);
-    vector<bool> nonNulledLineO(10, false);
+    vector<int> occupiedO(10, 0);
+    vector<bool> blockedO(10, false);
 
     //the first 4 are for the colunms, the following for the rows and the last for the diagonals
     for (int c = 0; c < 4; c++)
@@ -158,84 +143,82 @@ int Player::utility(const GameState &state)
             if (valueOfCell == player)
             {
                 //this is the cell has a value that is not empty and also not player Xs
-                nonNulledLineO[c] = true;
-                nonNulledLineO[4 + r] = true;
+                blockedO[c] = true;
+                blockedO[4 + r] = true;
                 if (r == c)
                 {
-                    nonNulledLineO[8] = true;
+                    blockedO[8] = true;
                 }
                 if (3 - r == c)
                 {
-                    nonNulledLineO[9] = true;
+                    blockedO[9] = true;
                 }
 
                 //this is if the value is the one that represents the players
-                differentSumsX[c] = (differentSumsX[c] + 1);
+                occupiedX[c] = (occupiedX[c] + 1);
 
-                differentSumsX[4 + r] = (differentSumsX[4 + r] + 1);
+                occupiedX[4 + r] = (occupiedX[4 + r] + 1);
                 if (r == c)
                 {
                     //this is for one of the diagonals
-                    differentSumsX[8] = (differentSumsX[8] + 1);
+                    occupiedX[8] = (occupiedX[8] + 1);
                 }
                 if (3 - r == c)
                 {
                     //this is for the other of the diagonals
-                    differentSumsX[9] = (differentSumsX[9] + 1);
+                    occupiedX[9] = (occupiedX[9] + 1);
                 }
             }
             else if (valueOfCell == notplayer)
             {
                 //this is if the value is the one that represents the players
-                differentSumsO[c] = (differentSumsO[c] + 1);
+                occupiedO[c] = (occupiedO[c] + 1);
 
-                differentSumsO[4 + r] = (differentSumsO[4 + r] + 1);
+                occupiedO[4 + r] = (occupiedO[4 + r] + 1);
                 if (r == c)
                 {
                     //this is for one of the diagonals
-                    differentSumsO[8] = (differentSumsO[8] + 1);
+                    occupiedO[8] = (occupiedO[8] + 1);
                 }
                 if (3 - r == c)
                 {
                     //this is for the other of the diagonals
-                    differentSumsO[9] = (differentSumsO[9] + 1);
+                    occupiedO[9] = (occupiedO[9] + 1);
                 }
 
                 //this is the cell has a value that is not empty and also not the players
-                nonNulledLineX[c] = true;
-                nonNulledLineX[4 + r] = true;
+                blockedX[c] = true;
+                blockedX[4 + r] = true;
                 if (r == c)
                 {
-                    nonNulledLineX[8] = true;
+                    blockedX[8] = true;
                 }
                 if (3 - r == c)
                 {
-                    nonNulledLineX[9] = true;
+                    blockedX[9] = true;
                 }
             }
         }
     }
-    int returnSumX = 0;
+    int SumX = 0;
     for (int i = 0; i < 10; i++)
     {
-        if (!nonNulledLineX[i])
+        if (!blockedX[i])
         {
-            returnSumX += differentSumsX[i];
+            SumX += occupiedX[i];
         }
     }
 
-    int returnSumO = 0;
+    int SumO = 0;
     for (int i = 0; i < 10; i++)
     {
-        if (!nonNulledLineO[i])
+        if (!blockedO[i])
         {
-            returnSumO += differentSumsO[i];
+            SumO += occupiedO[i];
         }
     }
 
-
-
-    return (returnSumX - returnSumO);
+    return (SumX - SumO);
 }
 
 /*namespace TICTACTOE*/ }
